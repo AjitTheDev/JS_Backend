be475eaa65da2f9b03a48f96a30a3d4500ffc17a
ajit sonu <ajit.shukla@aristaconsultingus.com>
2024-09-13 09:23:53 +0530

    DB and server connect

---
+++ backend/src/db/connection.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+import { DB_NAME } from '../constant.js'
+
+
+const connectDB=async () =>{
+    try{
+        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n DB name ${DB_NAME}`)
+        console.log(`\n DB connected ${connectInstance.connection.host}`)
+    }catch(error){
+        console.log(`connection failed, ${error}`)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import {app} from './app/app.js';
+import dotenv from 'dotenv';
+import connectDB from './db/connection.js';
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log('Error',error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log('Server running at PORT ' + process.env.PORT)})
+})
+.catch((err)=>{
+    console.log('Mongo DB connection failed !!', err)
+})
